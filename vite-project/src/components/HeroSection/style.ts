648b5a6db3cdedadc6bd6343fe9a947b4946c5fd
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-04-14 10:59:06 -0300

    feat: HeroSection
    - criação da estilização inicial do HeroSection;

--- vite-project/src/components/HeroSection/style.ts
+++ vite-project/src/components/HeroSection/style.ts
@@ -5,9 +5,28 @@ interface IHeroSectionStiledProps {
 }
 
 export const HeroSectionStiled = styled.div<IHeroSectionStiledProps>`
+  justify-content: center;
   background-image: url(${(props) => props.photo});
   background-size: cover;
   background-repeat: no-repeat;
 
   height: 548px;
+
+  .content {
+    justify-content: center;
+    gap: 16px;
+
+    max-width: 551px;
+
+    margin-left: 155px;
+
+    & > p {
+      max-width: 349px;
+    }
+
+    & > button {
+      margin-top: 16px;
+      
+    }
+  }
 `;
