8c7201fe5611e0c013a3e384c5f8d97891ae8202
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-04-17 18:37:47 -0300

    feat: carousel
    - criação do carousel do componente HeroSection
    - adicionado lib react slick

--- vite-project/src/components/HeroSection/HeroSection.tsx
+++ vite-project/src/components/HeroSection/HeroSection.tsx
@@ -1,35 +1,71 @@
 import { databaseMain } from "../../database/database";
-import { IDatababaseMain } from "../../interfaces/DatabaseInterface";
+import { IDatabaseMainProps } from "../../interfaces/DatabaseInterface";
 import { Heading, Paragraph } from "../../styles/typography";
 import { Button } from "../Button/Button";
 import { FixedTop } from "./FixedTop/FixedTop";
-import { HeroSectionStiled } from "./style";
-import { RxCaretUp } from "react-icons/rx";
+import { HeroSectionStiled, StyledSlider } from "./style";
 
 export const HeroSection = () => {
-  const mainData: IDatababaseMain = databaseMain[0];
-  return (
-    <HeroSectionStiled photo={mainData.src}>
-      <div className="content">
-        <Heading level={1} size={"xxxxxl"} lineHeight={"66px"}>
-          {mainData.title}
-        </Heading>
-        <Paragraph
-          fontWeight={400}
-          size={"lg"}
-          color={"white"}
-          lineHeight={"20px"}
-          fontFamily={"Barlow"}
-          textTransf={"none"}
-        >
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna
-        </Paragraph>
-        <Button buttonSize={"120px"} variant={"orange1"}>
-          Confira
-        </Button>
-        <FixedTop />
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    appendDots: (dots: any) => (
+      <div
+        style={{
+          color: "orange",
+          height: "20px", /* adicione a altura desejada aqui */
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          // position: "absolute",
+          top: "500px",
+          // padding: "10px",
+        }}
+      >
+        <ul style={{ margin: "0px", listStyle: "none", padding: "0px", }}> {dots} </ul>
       </div>
-    </HeroSectionStiled>
+    ),
+  };
+
+  return (
+    <StyledSlider {...settings}>
+      {databaseMain.map((data) => (
+        <div key={data.id}>
+          <HeroSectionSlide data={data} />
+        </div>
+      ))}
+    </StyledSlider>
   );
 };
+
+
+
+const HeroSectionSlide = ({ data }: IDatabaseMainProps) => (
+  <HeroSectionStiled photo={data.src}>
+    <div className="content">
+      <Heading level={1} size={"xxxxxl"} lineHeight={"66px"}>
+        {data.title}
+      </Heading>
+      <Paragraph
+        fontWeight={400}
+        size={"lg"}
+        color={"white"}
+        lineHeight={"20px"}
+        fontFamily={"Barlow"}
+        textTransf={"none"}
+      >
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna
+      </Paragraph>
+      <Button buttonSize={"120px"} variant={"orange1"}>
+        Confira
+      </Button>
+      <FixedTop />
+    </div>
+  </HeroSectionStiled>
+);
