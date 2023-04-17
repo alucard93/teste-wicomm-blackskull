0e99211edcb053bbab8b0271fc65e9f181f88c0b
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2023-04-17 04:39:12 -0300

    fix: deploy
    - remoção tipagem.

--- vite-project/src/components/PersonalObjectivesSlider/ObjectiveCard/ImageViewerSvg/ImageViewer.tsx
+++ vite-project/src/components/PersonalObjectivesSlider/ObjectiveCard/ImageViewerSvg/ImageViewer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function ImageViewer(props): any {
+export function ImageViewer() {
   return (
     <div>
       <svg
@@ -9,7 +9,7 @@ export function ImageViewer(props): any {
         viewBox="0 0 68 40"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        {...props}
+        
       >
         <path d="M40 0H52.5L32.5 40H20L40 0Z" fill="#FF9601" />
         <path d="M20 0H32.5L12.5 40H0L20 0Z" fill="#FF9601" />
